f80c7a37e14cddad6d09fc72a7bb250005426c7a
auone1328 <98250076+auone1328@users.noreply.github.com>
2022-11-07 21:30:47 +0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(scrollTo(0, 0))
+
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        pager: false, 
+        auto: true,
+    });
+}); 
+
+$(document).ready(function(){
+    var mixer = mixitup('.box-list');
+});
